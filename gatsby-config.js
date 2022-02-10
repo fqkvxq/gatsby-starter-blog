85e65291fe6dcfac9dafc932aac3799316966897
Takuya Kawai <takuyakawai32@gmail.com>
2022-02-11 03:48:44 +0900

    install dot env

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,6 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`
+})
 module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
@@ -19,6 +22,14 @@ module.exports = {
         path: `${__dirname}/content/blog`,
         name: `blog`,
       },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `gatsbyjs`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.API_KEY,
+      }
     },
     {
       resolve: `gatsby-source-filesystem`,
